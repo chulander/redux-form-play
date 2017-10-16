5e0857d3c652191c9594640cfd9d3fc262aafb2e
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2017-10-16 02:35:20 -0400

    setup complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import styles from './index.css';
+// import App from './App';
+import { Provider } from 'react-redux'
+import { Values } from 'redux-form-website-template'
+import store from './store'
+import showResults from './showResults'
+import InitializeFromStateForm from './InitilaizeFromStateForm'
+import 'font-awesome/css/font-awesome.min.css'
+const rootEl = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Initialize From State</h2>
+      <InitializeFromStateForm onSubmit={showResults} />
+      <Values form="initializeFromState" />
+    </div>
+  </Provider>,
+  rootEl
+);
+
